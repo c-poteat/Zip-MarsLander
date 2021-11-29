1ccb2e50c84f72ec64b5a6b9279282fb6a9014d6
Carnell <92267723+c-poteat@users.noreply.github.com>
2021-11-29 00:27:40 -0500

    Updated still not working

--- src/main/java/OnBoardComputer.java
+++ src/main/java/OnBoardComputer.java
@@ -1,24 +1,44 @@
-import sun.security.krb5.internal.crypto.Des;
-
 import java.util.concurrent.TimeUnit;
 
 public class OnBoardComputer implements BurnStream {
+    int burn = 0;
 
     @Override
     public int getNextBurn(DescentEvent status) throws InterruptedException {
 
-        int burn = 0;
-        if (status.getAltitude() < 20000 && status.getAltitude() > 6000) {
-            burn = 100;
-        } else if (status.getAltitude() < 6000 && status.getAltitude() >200 & status.getVelocity() > 0) {
-            burn = 200;
-        } else if(status.getVelocity() == 0) {
-            burn = 97;
-        } else if(status.getVelocity() > 2 && status.getVelocity() < 5) {
-            burn = 100;
+
+        if (status.getAltitude() > 5500) {
+            burnAmount100();
+
+        } else if (status.getVelocity() > 100 && status.getAltitude() < 5500) {
+            burnAmount200();
+
+        } else if (status.getVelocity() > 100) {
+            burnAmount100();
+
+        } else if (status.getAltitude() == 100 & status.getAltitude() < 200) {
+            burnAmount200();
+//            burnAmount200();
+//            burnAmount100();
+
         }
+
             TimeUnit.SECONDS.sleep(1);
             System.out.println(burn);
             return burn;
+
         }
+        public int burnAmount100 () {
+            burn = 100;
+            return burn;
+        }
+    public int burnAmount200 () {
+        burn = 200;
+        return burn;
     }
+    public int burnAmount98 () {
+        burn = 98;
+        return burn;
+    }
+    }
+
