9a578b08da2a61cfcb927600970da7ea501547d1
Carnell <92267723+c-poteat@users.noreply.github.com>
2021-11-28 21:40:13 -0500

    Updated lander

--- src/main/java/OnBoardComputer.java
+++ src/main/java/OnBoardComputer.java
@@ -8,11 +8,14 @@ public class OnBoardComputer implements BurnStream {
     public int getNextBurn(DescentEvent status) throws InterruptedException {
 
         int burn = 0;
-        if(status.getAltitude() < 20000 && status.getAltitude() > 5000) {
+        if (status.getAltitude() < 20000 && status.getAltitude() > 6000) {
             burn = 100;
-        } else if(status.getAltitude() < 5000 && status.getVelocity() > 0) {
-            burn = 197;
-
+        } else if (status.getAltitude() < 6000 && status.getAltitude() >200 & status.getVelocity() > 0) {
+            burn = 200;
+        } else if(status.getVelocity() == 0) {
+            burn = 97;
+        } else if(status.getVelocity() > 2 && status.getVelocity() < 5) {
+            burn = 100;
         }
             TimeUnit.SECONDS.sleep(1);
             System.out.println(burn);
